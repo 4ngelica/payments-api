f17a42d2a6a73c51ff6456e5a3fa77ea3ea89ad4
Angélica Nunes <angelica.nunes@usp.br>
2021-12-12 19:21:07 -0300

    feat: add http responses

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -3,42 +3,95 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use App\Models\Transaction;
-
 use App\Models\User;
 
 class TransactionController extends Controller
 {
+    /**
+    * TransactionController constructor.
+    * @param Transaction $transaction
+    */
     public function __construct(Transaction $transaction)
     {
       $this->transaction = $transaction;
     }
 
-    public function store(Request $request) {
-        $pendingTransaction = $request->validate($this->transaction->rules(), $this->transaction->feedback());
-        if($this->transaction->authorizePayerType($request->payer_id)){
-            $pendingTransaction = $this->transaction->create($pendingTransaction);
-
-            if($this->transaction->authorizeBalance($request->payer_id, $request->value)){
-                $this->transaction->beginTransaction($request->payer_id, $request->payee_id, $request->value);
-                    if($this->transaction->authorizeExternalService($pendingTransaction)){
-                        return response()->json(['ok' => 'Successful operation']);
-                    }else{
-                        $this->transaction->revertTransaction($request->payer_id, $request->payee_id, $request->value);
-                        return response()->json(['Error' => 'Non authorized by external service']);
-                    }
-            }
-        }
-        return response()->json(['Error' => 'Non authorized']);
-    }
-
-    public function index() {
+    /**
+    * Responds to a GET request into
+    * /transaction endpoint with all transactions
+    *
+    * @return JsonResponse
+    */
+    public function index(): JsonResponse
+    {
         $transaction = $this->transaction->all();
         return response()->json($transaction);
     }
 
-    public function show($id) {
+    /**
+    * Responds to a GET request into
+    * /transaction/{id} endpoint with the
+    * respective transaction
+    *
+    * @return JsonResponse
+    * @param Int $id
+    */
+    public function show($id): JsonResponse
+    {
         $transaction = $this->transaction->find($id);
         return response()->json($transaction);
+    }
+
+    /**
+    * Handle a POST request into
+    * /transaction endpoint
+    *
+    * @return JsonResponse
+    * @param Request $request
+    */
+    public function store(Request $request): JsonResponse
+    {
+        $this->transaction->payer();
+        $payerId = $request->payer_id;
+        $payeeId = $request->payee_id;
+        $value = $request->value;
+
+        $pendingTransaction = $request->validate(
+            $this->transaction->rules(),
+            $this->transaction->feedback()
+        );
+
+        if ( $this->transaction->authorizePayerType($payerId) ) {
+
+            $pendingTransaction = $this->transaction
+                ->create($pendingTransaction);
+
+            if( $this->transaction->authorizeBalance($payerId, $value) ) {
+                $this->transaction->beginTransaction($payerId, $payeeId, $value);
+
+                if( $this->transaction->authorizeExternalService($pendingTransaction) ) {
+                    return response()->json(
+                        ['ok' => 'Successful operation'],
+                        JsonResponse::HTTP_OK
+                    );
+                }else {
+                    $this->transaction->revertTransaction($payerId, $payeeId, $value);
+                    return response()->json(
+                        ['Error' => 'Non authorized by external service'],
+                        JsonResponse::HTTP_UNPROCESSABLE_ENTITY
+                    );
+                }
+            }
+            return response()->json(
+                ['Error' => 'This user does not have enough ballance to make this transaction'],
+                JsonResponse::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
+        return response()->json(
+            ['Error' => 'This user cannot make transactions'],
+            JsonResponse::HTTP_UNPROCESSABLE_ENTITY
+        );
     }
 }
