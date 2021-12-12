270ad8101e34756e2a1c3af0a48ea0ba15dd793d
Angélica Nunes <angelica.nunes@usp.br>
2021-12-11 22:39:24 -0300

    feat: add authorizePayerType function

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
@@ -31,5 +33,16 @@ class Transaction extends Model
 
     public function payer() {
         return $this->belongsTo('App\Models\User');
+    }
+
+    public function authorizePayerType($payer) {
+
+        $payerType = User::where('id', $payer)->first()->getAttribute('type');
+        
+        if($payerType == 1){
+            return false;
+        }
+
+        return true;
     }
 }
