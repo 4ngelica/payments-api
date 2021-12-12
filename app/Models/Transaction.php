5a260986f76dc845b3232c8ef0dec911d2210510
Angélica Nunes <angelica.nunes@usp.br>
2021-12-12 19:08:09 -0300

    feat: define return types

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -13,6 +13,7 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     const URL = 'https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6';
+    var $errors = [];
 
     use HasFactory;
     protected $fillable = [
@@ -22,7 +23,13 @@ class Transaction extends Model
         'status'
     ];
 
-    public function rules() {
+    /**
+    * Returns an array with the request rules.
+    *
+    * @return array
+    */
+    public function rules() : array
+    {
       return [
         'payer_id'=> 'required',
         'payee_id' => 'required',
@@ -30,44 +37,104 @@ class Transaction extends Model
       ];
     }
 
-    public function feedback() {
+    /**
+    * Returns an array with the feedback messages.
+    *
+    * @return array
+    */
+    public function feedback() : array
+    {
       return [
         'required' => 'The :attribute field is required'
       ];
     }
 
-    public function payer() {
+    /**
+    * Set the transaction/user relationship
+    *
+    * @return Illuminate\Database\Eloquent\Relations\BelongsTo
+    */
+    public function payer()
+    {
         return $this->belongsTo('App\Models\User');
     }
 
-    public function authorizePayerType($payer) {
-
-        $payerType = User::where('id', $payer)->first()->getAttribute('type');
+    /**
+    * Validates a transaction request based
+    * on its payer type, by returning
+    * a boolean value
+    *
+    * @return bool
+    */
+    public function authorizePayerType($payerId) : bool
+    {
+        $payerType = User::where('id', $payerId)->first()->getAttribute('type');
         if($payerType == 1){
+            $errors = ['teste'];
             return false;
         }
         return true;
     }
 
-    public function authorizeBalance($payer, $value) {
-
-        $payerBalance = User::with('wallet')->where('id', $payer)->first()->getRelation('wallet')->getAttribute('balance');
+    /**
+    * Validates a transaction request based
+    * on payer's wallet balance, by returning
+    * a boolean value
+    *
+    * @return bool
+    */
+    public function authorizeBalance($payerId, $value) : bool
+    {
+        $payerBalance = User::with('wallet')->where('id', $payerId)->first()->getRelation('wallet')->getAttribute('balance');
         if($payerBalance >= $value ){
             return true;
         }
         return false;
     }
 
-    public function beginTransaction($payer, $payee, $value) {
-        $payerBalance = User::with('wallet')->where('id', $payer)->first()->getRelation('wallet');
+    /**
+    * Begin a transaction by decrementing
+    * the value from payer's wallet
+    * and incrementing it into payee's wallet
+    *
+    * @return void
+    */
+    public function beginTransaction($payerId, $payeeId, $value) : void
+    {
+        $payerBalance = User::with('wallet')->where('id', $payerId)->first()->getRelation('wallet');
         $payerBalance->setAttribute( 'balance', $payerBalance->getAttribute('balance') - floatval($value))->save();
 
-        $payeeBalance = User::with('wallet')->where('id', $payee)->first()->getRelation('wallet');
+        $payeeBalance = User::with('wallet')->where('id', $payeeId)->first()->getRelation('wallet');
         $payeeBalance->setAttribute( 'balance', $payeeBalance->getAttribute('balance') + floatval($value))->save();
     }
 
-    public function authorizeExternalService($pendingTransaction) {
+    /**
+    * Revert a transaction by decrementing
+    * the value from payee's wallet
+    * and incrementing it into payers wallet
+    *
+    * @return void
+    */
+    public function revertTransaction($payerId, $payeeId, $value) : void
+    {
+        $payerBalance = User::with('wallet')->where('id', $payerId)->first()->getRelation('wallet');
+        $payerBalance->setAttribute( 'balance', $payerBalance->getAttribute('balance') + floatval($value))->save();
 
+        $payeeBalance = User::with('wallet')->where('id', $payeeId)->first()->getRelation('wallet');
+        $payeeBalance->setAttribute( 'balance', $payeeBalance->getAttribute('balance') - floatval($value))->save();
+    }
+
+    /**
+    * Validates a transaction request
+    * by sending a GET request to
+    * an external validation service
+    * and sets a 'completed' transaction
+    * status if is authorized
+    *
+    * @return bool
+    */
+    public function authorizeExternalService($pendingTransaction) : bool
+    {
         $response = Http::get(self::URL);
         $response = collect(json_decode($response, true));
         $serviceResponse = Arr::get($response, 'message');
@@ -79,13 +146,5 @@ class Transaction extends Model
 
         $pendingTransaction->setAttribute( 'status', 'canceled')->save();
         return false;
-    }
-
-    public function revertTransaction($payer, $payee, $value){
-        $payerBalance = User::with('wallet')->where('id', $payer)->first()->getRelation('wallet');
-        $payerBalance->setAttribute( 'balance', $payerBalance->getAttribute('balance') + floatval($value))->save();
-
-        $payeeBalance = User::with('wallet')->where('id', $payee)->first()->getRelation('wallet');
-        $payeeBalance->setAttribute( 'balance', $payeeBalance->getAttribute('balance') - floatval($value))->save();
     }
 }
