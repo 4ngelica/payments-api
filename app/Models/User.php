7a40d642a6319456c89e34ced88290ba63527f68
Angélica Nunes <angelica.nunes@usp.br>
2021-12-12 19:07:01 -0300

    feat: add return type

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\hasMany;
+use Illuminate\Database\Eloquent\Relations\hasOne;
 
 class User extends Model
 {
@@ -17,11 +19,23 @@ class User extends Model
     'type'
   ];
 
-  public function transactions() {
+  /**
+  * Set the user/transactions relationship
+  *
+  * @return Illuminate\Database\Eloquent\Relations\hasMany
+  */
+  public function transactions() : hasMany
+  {
       return $this->hasMany('App\Models\Transaction', 'foreign_key', 'payer_id');
   }
 
-  public function wallet() {
+  /**
+  * Set the user/wallet relationship
+  *
+  * @return Illuminate\Database\Eloquent\Relations\hasOne
+  */
+  public function wallet() : hasOne
+  {
       return $this->hasOne('App\Models\Wallet');
   }
 }
